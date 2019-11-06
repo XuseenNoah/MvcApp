d4f4cc4158f512f4a6ad04f91df34574898ee951
xuseen muse <xuseens@live.com>
2019-11-06 10:03:44 +0300

    first commit

---
+++ MvcApplication/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        public ActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        public ActionResult GetEveryOne()
+        {
+            //return Content("Hello World This is my wold");
+            //return new EmptyResult();
+            return HttpNotFound();
+        }
+
+
+
+
+        // GET: Person
+   
+    }
+}
